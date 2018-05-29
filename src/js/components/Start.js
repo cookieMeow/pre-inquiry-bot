9d59ca27be1c89b97964fd2dd694f27081fd0d2d
cwang100 <cwang100@illinois.edu>
2018-05-29 23:43:11 +0800

    add read card page

--- src/js/components/Start.js
+++ src/js/components/Start.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
 import Home from './Home.js';
+import ReadCard from './ReadCard.js';
 import StartContainer from './StartContainer.js';
 import '../../css/Button.css';
 
 class Inquiry extends Component {
 	render() {
 		return (
-
-			<StartContainer>
-				<Home />
-			</StartContainer>
-
+			<Router>
+				<StartContainer>
+					<Route exact path="/" component={Home}/>
+					<Route path="/readcard" component={ReadCard}/>
+				</StartContainer>
+			</Router>
 		);
 	}
 }
