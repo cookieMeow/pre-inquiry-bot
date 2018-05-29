1023f4636b638b2633904d1651269916987ce80a
cwang100 <cwang100@illinois.edu>
2018-05-30 00:44:51 +0800

    fix readcard

--- src/js/components/ReadCard.js
+++ src/js/components/ReadCard.js
@@ -5,13 +5,27 @@ import ButtonNext from './ButtonNext.js'
 import '../../css/ReadCard.css';
 
 class ReadCard extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { Text: "请刷医保卡" };
+
+		// This binding is necessary to make `this` work in the callback
+		this.handleEvent = this.handleEvent.bind(this);
+	}
+
+	handleEvent(event) {
+		this.setState(prevState => ({
+			Text: "刷卡成功"
+		}));
+	}
+
 	render() {
 		return (
-			<div className="read-card-container">
+			<div className="read-card-container" onMouseDown={ this.handleEvent }>
 				<div className="banner">
 					<Row>
 						<Col>
-							<p>请刷医保卡</p>
+							<p>{this.state.Text}</p>
 						</Col>
 					</Row>
 				</div>
